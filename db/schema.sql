08648101e2ad2f6702aea4a012576be96d53aa23
ThePalad1n <31965502+ThePalad1n@users.noreply.github.com>
2020-05-27 20:01:50 -0400

    before break commit

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,14 @@ CREATE DATABASE zzmb7cc7fazm5iny;
 
 USE zzmb7cc7fazm5iny;
 
-
 CREATE TABLE IF NOT EXISTS accounts (
   id int(11) NOT NULL,
   username varchar(50) NOT NULL,
   pass varchar(255) NOT NULL,
   email varchar(100) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO accounts (id, username, pass, email) VALUES (1, test, test, test@test.com);
 
 ALTER TABLE accounts ADD PRIMARY KEY (id);
 ALTER TABLE accounts MODIFY id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
