3a470bac4616d8947d64f8694e6f392d120b1390
ThePalad1n <31965502+ThePalad1n@users.noreply.github.com>
2020-05-30 11:21:36 -0400

    orm added server changes

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,6 @@ USE jobmancer;
 
 CREATE TABLE IF NOT EXISTS accounts (
   id int(11) NOT NULL,
-  username varchar(50) NOT NULL,
   pass varchar(255) NOT NULL,
   email varchar(100) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
