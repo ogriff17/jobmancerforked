2fd95464c948bdcf9bb7f8c0338f5b2f56bfd07b
ThePalad1n <31965502+ThePalad1n@users.noreply.github.com>
2020-06-01 20:01:52 -0400

    form 1 added other minor html and js changes

--- server.js
+++ server.js
@@ -126,6 +126,18 @@ app.get('/login', (req, res) => {
 app.get('/signup', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'signup.html'));
 });
+app.get('/mancerpt1', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'mancerpt1.html'));
+});
+app.get('/mancerpt2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'mancerpt2.html'));
+});
+app.get('/mancerpt3', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'mancerpt3.html'));
+});
+
+
+
 
 // =============================================================================
 // LISTENER
