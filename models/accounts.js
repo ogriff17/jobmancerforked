26127cd4cae09bc9170e6ec79f8747d8a9592e9d
ThePalad1n <31965502+ThePalad1n@users.noreply.github.com>
2020-06-03 19:16:41 -0400

    changes

--- models/accounts.js
+++ models/accounts.js
@@ -1,6 +1,6 @@
-var Sequelize = require("sequelize");
 
-var sequelize = require("../connections/connection.js")
+var Sequelize = require("sequelize");
+var sequelize = require("../connections/connections");
 
 var Users = sequelize.define("accounts", {
     email: Sequelize.STRING,
@@ -8,5 +8,33 @@ var Users = sequelize.define("accounts", {
 });
 
 Users.sync();
+
+
+var acc = {
+    fetchPasswordForUser: function (email, password, cb) {
+        var queryString = "SELECT * FROM accounts where email = ? AND pass = ?";
+        connection.query(queryString, [email, password], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result)
+        });
+    },
+
+    createAccount: function (email, password, cb) {
+        var queryString = "INSERT INTO accounts where email = ? AND pass = ? AND";
+        connection.query(queryString, [email, password], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result)
+        });
+    },
+    
+};
+
+
+
+module.exports = orm;
 
 module.exports = Accounts;
