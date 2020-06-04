66b4746c86b21fa57ba0e494c50d48463cd5eaf9
ThePalad1n <31965502+ThePalad1n@users.noreply.github.com>
2020-06-03 21:42:28 -0400

    Delete users-controller.js

--- controllers/users-controller.js
+++
@@ -1,9 +0,0 @@
-var Express = require("express");
-
-var Router = express.Router();
-
-var Users = require("../models/users.js");
-
-Router.get("/", function(req, res) {
-    res.redirect("/mancerpt1")
-})
