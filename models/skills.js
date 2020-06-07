fd18865960ccdacf79bd6f0d1e337c6335775d70
ThePalad1n <31965502+ThePalad1n@users.noreply.github.com>
2020-06-07 18:34:36 -0400

    more skills added and new color and more and and a

--- models/skills.js
+++ models/skills.js
@@ -11,7 +11,55 @@ module.exports = (sequelize, DataType) => {
        keyword: {
           type: DataType.STRING,
           allowNull: false
-       }
+       },
+       skillName2: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      skillLevel2: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      keyword2: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      skillName3: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      skillLevel3: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      keyword3: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      skillName4: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      skillLevel4: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      keyword4: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      skillName5: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      skillLevel5: {
+         type: DataType.STRING,
+         allowNull: false
+      },
+      keyword5: {
+         type: DataType.STRING,
+         allowNull: false
+      }
     })
     return Skills;
  };
